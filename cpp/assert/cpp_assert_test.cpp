7a29eeb468f336cfd400685acebbcfd4c0944432
Samuel Casa <samuelcasa9@gmail.com>
2013-02-06 13:49:59 +0100

    c++ assert test added

---
+++ cpp/assert/cpp_assert_test.cpp
@@ -0,0 +1,49 @@
+
+
+//define this before assert.h include to disable the assert macro
+#define NDEBUG
+
+
+
+#include <iostream>
+#include <assert.h>
+
+
+using namespace std;
+ 
+
+
+class Crap
+{
+
+	private:
+		int nothing;
+
+	public:
+		Crap()
+		{
+			nothing = 0;
+		}
+
+		void addNumber(int* number)
+		{
+			cout << " > before assert" << endl;
+			assert(number != NULL);
+			cout << " > after assert" << endl;
+			nothing = nothing + *number;
+		}
+
+		int getNothing()
+		{
+			return nothing;
+		}	
+
+};
+
+
+int main()
+{
+	Crap crap;
+	crap.addNumber(NULL);
+	cout << "Hello World!: " << crap.getNothing() << endl;
+}

77d46246d64d66ad25fdec31bbadf5d1f1c6331c
Samuel Casa <samuelcasa9@gmail.com>
2013-04-16 21:42:30 +0200

    designated initializers example added in c

--- c/designated_initializers/main.c
+++ c/designated_initializers/main.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
-#include "source1.h"
 
-//extern is here the default...
-extern int source_one_int_extern;
-
-static int source_one_int_static = 13;
-
-void say_hello_from_extern_main(void)
+typedef struct
 {
-    printf("[%s] hello extern!\n", __func__);
-    printf(" * pointer to source_one_int_extern: %p\n", &source_one_int_extern);
-    printf(" * value of source_one_int_extern: %d\n", source_one_int_extern);
+    char* name;
+    int left;
+    int right;
+    int up;
+    int down;
+} direction_s;
+
+
+void this_row(direction_s d)
+{
+    printf( d.left      ?   "*..\n"
+            : d.right   ?   "..*\n"
+            :               ".*.\n");
 }
 
-void say_hello_from_static_main(void)
+
+void draw_box(direction_s d)
 {
-    printf("[%s] hello static!\n", __func__);
-    printf(" * pointer to source_one_int_static: %p\n", &source_one_int_static);
-    printf(" * value of source_one_int_static: %d\n", source_one_int_static);
+    printf("%s:\n", (d.name ? d.name : "a box"));
+    d.up                    ? this_row(d) : printf("...\n");
+    (!d.up && !d.down)      ? this_row(d) : printf("...\n");
+    d.down                  ? this_row(d) : printf("...\n");
+    printf("\n");
 }
+
 
 int main(void)
 {
     printf("[%s] hello world\n", __func__);
 
-    say_hello_from_extern_source1();
+    direction_s D = {.name = "left", .left = 1};
+    draw_box(D);
 
-    //extern linkage
-    source_one_int_extern = 42;
-    say_hello_from_extern_main();
-    say_hello_from_extern_source1();
+    D = (direction_s){"upper right", .up = 1, .right = 1};
+    draw_box(D);
 
-    //static linkage
-    say_hello_from_static_main();
-    say_hello_from_static_source1();
-    
+    draw_box((direction_s){});
+
     return 0;
 }
 

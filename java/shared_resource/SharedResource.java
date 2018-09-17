c0c72a6ee64f0203b5be9c3a6a8096caf1d82852
Samuel Casa <samuelcasa9@gmail.com>
2018-09-17 06:22:15 +0200

    fixup! java: Add shared resource application

--- java/shared_resource/SharedResource.java
+++ java/shared_resource/SharedResource.java
@@ -1,10 +1,36 @@
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.logging.Logger;
+import java.util.logging.LogManager;
+import java.io.InputStream;
+import java.io.IOException;
+
+
+class MyLogger {
+    protected static Logger LOGGER = null;
+    private static String loggingProps = "logging.properties";
+
+    static {
+        InputStream stream = MyLogger.class.getClassLoader().
+                getResourceAsStream(loggingProps);
+        try {
+            LogManager.getLogManager().readConfiguration(stream);
+            LOGGER = Logger.getLogger(MyLogger.class.getName());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+// TODO: HOW TO AVOID THIS???
+class MyLoggingThread extends MyLogger {
+
+}
 
 class Factory2<T> {
 
-    // T type MUST have a default constructor
+    //NOTE: T type MUST have a default constructor
     private final Class<T> type;
 
     public Factory2(Class<T> type) {
@@ -25,18 +51,33 @@ class Factory2<T> {
     }
 }
 
-class WorkerThreadPool<T> {
+class WorkerThreadPool<T> extends MyLoggingThread {
 
     private final Class<T> type;
     private ArrayList<T> workerThreads;
     private int threadPoolSize;
-    private Logger logger;
 
     WorkerThreadPool(Class<T> type, int threadPoolSize) {
         this.type = type;
         threadPoolSize = threadPoolSize;
-        logger = Logger.getLogger(getClass().getName());
         workerThreads = new ArrayList<>();
+
+        //FIXME: This has to be here, I can not put that to a private function... how to fix that ??
+        for (int i = 0; i < threadPoolSize; i++) {
+            Factory2<T> factory = Factory2.getInstance(type);
+            workerThreads.add(factory.getInstance());
+        }
+        initThreadPool();
+    }
+
+    public static <F> WorkerThreadPool<F> getInstance(Class<F> type, int threadPoolSize) {
+        return new WorkerThreadPool<F>(type, threadPoolSize);
+    }
+
+    //FIXME!!!
+    private void initThreadPool() {
+
+//        LOGGER.fine(String.format("thread pool size: %d", threadPoolSize));
 
         for (int i = 0; i < threadPoolSize; i++) {
             Factory2<T> factory = Factory2.getInstance(type);
@@ -44,24 +85,30 @@ class WorkerThreadPool<T> {
         }
     }
 
-    public static <F> WorkerThreadPool<F> getInstance(Class<F> type, int threadPoolSize) {
-        return new WorkerThreadPool<F>(type, threadPoolSize);
+
+    public void run() {
+
+
+        LOGGER.fine("Let's process some data...");
+
+
     }
 
 
 }
 
-class WorkerThread extends Thread {
+
+class WorkerThread extends MyLoggingThread {
 
     private static int objCount;
     protected final int upperRandLimit = 50;
-    protected Logger logger;
+    //    protected Logger logger;
     private int nrOfProcessedObjects = 0;
-    private String uniqueIdentifier;
+//    private String uniqueIdentifier;
 
 
     WorkerThread() {
-        uniqueIdentifier = String.format("%s_%d_%s", getClass().getName(), objCount, hashCode());
+//        uniqueIdentifier = String.format("%s_%d_%s", getClass().getName(), objCount, hashCode());
         objCount++;
     }
 
@@ -75,30 +122,21 @@ class WorkerThread extends Thread {
     }
 
     public String getUniqueIdentifier() {
-        return uniqueIdentifier;
+        return this.toString();
     }
 
 }
 
 
-class ConsumerThread extends WorkerThread {
-    private Logger logger;
-
-    ConsumerThread() {
-        logger = Logger.getLogger(getClass().getName());
-        logger.fine(String.format("New %s: %d @ %s", getClass().getName(), getThreadCount(), this));
-    }
-}
-
-class SynchronizedData {
+class SynchronizedData extends MyLogger {
 
 //    private List<String> syncal;
 
-    private Logger logger;
+    //    private Logger logger;
     private ArrayList<Integer> data;
 
     SynchronizedData() {
-        logger = Logger.getLogger(getClass().getName());
+//        logger = Logger.getLogger(getClass().getName());
 
         //        syncal = Collections.synchronizedList(new ArrayList<String>());
         data = new ArrayList<>();
@@ -109,7 +147,7 @@ class SynchronizedData {
     public void add(int dataElement) {
         synchronized (data) {
 
-            logger.fine(String.format("Add data element %d", dataElement));
+            LOGGER.fine(String.format("Add data element %d", dataElement));
             data.add(dataElement);
         }
     }
@@ -118,12 +156,12 @@ class SynchronizedData {
     public void get() {
         synchronized (data) {
             if (data.isEmpty()) {
-                logger.fine("No data to get");
+                LOGGER.fine("No data to get");
                 return;
             }
 
             int d = data.get(0);
-            logger.fine(String.format("Get data element %d", d));
+            LOGGER.fine(String.format("Get data element %d", d));
         }
     }
 
@@ -143,12 +181,21 @@ class SynchronizedData {
 }
 
 
+class ConsumerThread extends WorkerThread {
+//    private Logger logger;
+
+    ConsumerThread() {
+//        logger = Logger.getLogger(getClass().getName());
+        LOGGER.fine(String.format("New %s: %d @ %s", getClass().getName(), getThreadCount(), getUniqueIdentifier()));
+    }
+}
+
 class ProducerThread extends WorkerThread {
 
 
     ProducerThread() {
-        logger = Logger.getLogger(getClass().getName());
-        logger.info(String.format("New %s: %d @ %s", getClass().getName(), getThreadCount(), this));
+//        logger = Logger.getLogger(getClass().getName());
+        LOGGER.fine(String.format("New %s: %d @ %s", getClass().getName(), getThreadCount(), getUniqueIdentifier()));
     }
 
     private void produceData() {
@@ -166,54 +213,57 @@ class ProducerThread extends WorkerThread {
     }
 
     public void run() {
-        logger.info(String.format("Starting %s", getUniqueIdentifier()));
+        LOGGER.info(String.format("Starting %s", getUniqueIdentifier()));
     }
 
 }
 
 
-class SharedResourceAccess {
+class SharedResourceAccess extends MyLogger {
 
-    static final int NR_OF_PRODUCER_THREADS = 7;
-    static final int NR_OF_CONSUMER_THREADS = 7;
-    private final int sleepTimeinSec = 5;
-    private final int producerThreadPoolSize = 10;
-    private final int consumerThreadPoolSize = 10;
-    private Logger logger;
+    private static final int NR_OF_PRODUCER_THREADS = 7;
+    private static final int NR_OF_CONSUMER_THREADS = 7;
+    private final int sleepTimeInSec;
+    //    static final int producerThreadPoolSize = 10;
+//    static final int consumerThreadPoolSize = 10;
+//    private Logger logger;
     private ArrayList<WorkerThreadPool> threadPools;
     private boolean running;
 
     SharedResourceAccess() {
-        logger = Logger.getLogger(getClass().getName());
+//        logger = Logger.getLogger(getClass().getName());
         threadPools = new ArrayList<WorkerThreadPool>();
-        WorkerThreadPool.getInstance(ProducerThread.class, producerThreadPoolSize);
-        threadPools.add(WorkerThreadPool.getInstance(ConsumerThread.class, consumerThreadPoolSize));
-        threadPools.add(WorkerThreadPool.getInstance(ProducerThread.class, consumerThreadPoolSize));
+
+
         running = true;
+        sleepTimeInSec = 5;
     }
 
     public static void main(String[] args) {
         SharedResourceAccess sharedResourceAccess = new SharedResourceAccess();
-        sharedResourceAccess.run();
+        sharedResourceAccess.operate();
     }
 
-    public void run() {
+    public void operate() {
 
-        for (WorkerThreadPool threadPool: threadPools) {
-//            threadPool.work();
+        LOGGER.info("Create worker thread pools");
+        threadPools.add(WorkerThreadPool.getInstance(ConsumerThread.class, NR_OF_CONSUMER_THREADS));
+        threadPools.add(WorkerThreadPool.getInstance(ProducerThread.class, NR_OF_PRODUCER_THREADS));
+        for (WorkerThreadPool threadPool : threadPools) {
+            threadPool.run();
         }
 
         while (running) {
-            logger.info(String.format("Process data for %ss", sleepTimeinSec));
+            LOGGER.info(String.format("Process data for %ss", sleepTimeInSec));
             try {
-                Thread.sleep(sleepTimeinSec * 1000);
+                Thread.sleep(sleepTimeInSec * 1000);
             } catch (InterruptedException ex) {
                 Thread.currentThread().interrupt();
             }
 
 
-
-        };
+        }
+        ;
     }
 
 

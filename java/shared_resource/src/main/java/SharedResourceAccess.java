68b0a141954bbda0ad4c00b72a5e71938c546ad9
Samuel Casa <samuelcasa9@gmail.com>
2018-10-05 11:29:52 +0200

    java: Shared resource application: Rework class SharedResourceAccess
    Signed-off-by: Samuel Casa <samuelcasa9@gmail.com>

--- java/shared_resource/src/main/java/SharedResourceAccess.java
+++ java/shared_resource/src/main/java/SharedResourceAccess.java
@@ -3,18 +3,19 @@ package src.main.java;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class SharedResourceAccess {
 
-    private static final String propFileName = "app.properties";
+    private static final String PROP_FILE_NAME = "app.properties";
 
-    private ArrayList<WorkerPool> workerPools;
+    private List<WorkerPool> workerPools;
     private SynchronizedData dataStore;
 
     private Logger logger;
 
-    static volatile boolean running = true;
+    static boolean running = true;
     static long statCnt = 0;
 
     MyConfig config;
@@ -35,7 +36,7 @@ public class SharedResourceAccess {
 
     private void initConfigFromPropFile() {
         ClassLoader loader = SharedResourceAccess.class.getClassLoader();
-        URL url = loader.getResource(propFileName);
+        URL url = loader.getResource(PROP_FILE_NAME);
         String absFileName = url.getFile();
         config = new MyConfig(absFileName);
         new InfoBanner(logger, config);
